e19232a69bb69a9b11de55ffd4a5f8f21e617a2e
Woler <hristo_den@abv.bg>
2022-04-19 22:08:38 +0300

    added Table Creation Scripts

---
+++ FishingAgency/Scripts/dbo.Table_Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [ShipId] INT NOT NULL
+)
