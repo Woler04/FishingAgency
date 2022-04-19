ab485c5ea4c42e3644d5e7a85ad7d86c976f4c4e
resrvationmanager <sonity.main@gmail.com>
2022-04-19 13:55:32 +0300

    This is the design of our register form!

---
+++ FishingAgency/View/LoginForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FishingAgency.View
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Register = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Register
+            // 
+            this.Register.AutoSize = true;
+            this.Register.BackColor = System.Drawing.Color.Transparent;
+            this.Register.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Register.Location = new System.Drawing.Point(170, 56);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(135, 45);
+            this.Register.TabIndex = 0;
+            this.Register.Text = "Register";
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(136, 222);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(273, 26);
+            this.Username.TabIndex = 1;
+            this.Username.Text = "                Username or e-mail                                           ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(136, 284);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "                        Password                                                 " + "   ";  // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.BackColor = System.Drawing.Color.Transparent;
+            this.Email.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Email.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.ForeColor = System.Drawing.Color.White;
+            this.Email.Location = new System.Drawing.Point(46, 220);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(71, 30);
+            this.Email.TabIndex = 3;
+            this.Email.Text = "Email";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.BackColor = System.Drawing.Color.Transparent;
+            this.Password.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Password.Location = new System.Drawing.Point(41, 284);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(92, 25);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Password";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FishingAgency.Properties.Resources.hubava_voda;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(488, 450);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Register);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Register;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Password;
+    }
+}
