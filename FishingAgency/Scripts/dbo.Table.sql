ac934048ccc02f2c984fcabce8f1e829fccf6e5a
Woler <hristo_den@abv.bg>
2022-04-19 21:56:01 +0300

    added User Table

---
+++ FishingAgency/Scripts/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FishingShips]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [LicenseExpiration] DATE NOT NULL, 
+    [isForHobby] BIT NOT NULL, 
+    [FuelUsage] FLOAT NOT NULL
+)
