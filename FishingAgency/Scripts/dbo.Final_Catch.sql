b28583d4604abdfa456cb3e0833bd68427751783
Woler <57759807+Woler04@users.noreply.github.com>
2022-04-26 13:20:38 +0300

    Delete dbo.Final_Catch.sql

--- FishingAgency/Scripts/dbo.Final_Catch.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Catch] (
-    [Id]        INT        NOT NULL,
-    [Amount]    FLOAT (53) NOT NULL,
-    [Lenght]    FLOAT (53) NOT NULL,
-    [StartDate] DATE       NOT NULL,
-    [ShipId]    INT        NOT NULL FOREIGN KEY ([ShipId]) REFERENCES FishingShips([Id]),
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
