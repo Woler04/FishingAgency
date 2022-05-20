f73d2f0e2a8ae69c863f56c3e70ba2d954455e88
Woler <hristo_den@abv.bg>
2022-05-20 22:21:07 +0300

    new Login View

--- FishingAgency/Program.cs
+++ FishingAgency/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FishingAgency.View;
 
 namespace FishingAgency
 {
@@ -16,7 +17,7 @@ namespace FishingAgency
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DataPreview());
+            Application.Run(new LoginView());
         }
     }
 }
